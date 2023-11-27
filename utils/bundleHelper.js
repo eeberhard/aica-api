592574c018d20b4bf720505764f6ee4d6ea94233
Yihsin Jen <71389754+jjenscodee@users.noreply.github.com>
2023-11-27 16:53:14 +0100

    fix: bundling issues with application schema (#77)
    * fix: avoid symbols being changed
    
    * fix: use decodeURI

--- utils/bundleHelper.js
+++ utils/bundleHelper.js
@@ -3,14 +3,13 @@ import * as fs from "fs";
 
 let filePath = process.argv[2];
 let outputFileName = process.argv[3];
-$RefParser.bundle(filePath).then((schema) => {
-    let jsonSchema = JSON.stringify(schema, null, 2);
+$RefParser.bundle(filePath).then(async (schema) => {
+    let jsonSchema = decodeURI(JSON.stringify(schema, null, 2));
     jsonSchema = jsonSchema.replaceAll('%24', '$');
     fs.writeFile(outputFileName, jsonSchema, 'utf8', (err) => {
-      if (err) throw err;
-      console.log('The file has been saved!');
+        if (err) throw err;
+        console.log('The file has been saved!');
     });
 });
-
 
 
