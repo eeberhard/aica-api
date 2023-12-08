ba49aab0083a4748be7519451248f95499926260
Yihsin Jen <71389754+jjenscodee@users.noreply.github.com>
2023-12-08 14:46:51 +0100

    fix: dereference schema (#83)
    * fix: dereference schema
    
    * fix: update ref
    
    * fix: resolve conversation

--- utils/bundleHelper.js
+++ utils/bundleHelper.js
@@ -3,13 +3,26 @@ import * as fs from "fs";
 
 let filePath = process.argv[2];
 let outputFileName = process.argv[3];
+
 $RefParser.bundle(filePath).then(async (schema) => {
+
     let jsonSchema = decodeURI(JSON.stringify(schema, null, 2));
     jsonSchema = jsonSchema.replaceAll('%24', '$');
-    fs.writeFile(outputFileName, jsonSchema, 'utf8', (err) => {
-        if (err) throw err;
-        console.log('The file has been saved!');
-    });
+
+    if(outputFileName === "application.schema.json") {
+        fs.writeFile(outputFileName, jsonSchema, 'utf8', (err) => {
+              if (err) throw err;
+              console.log('The file has been saved!');
+        });
+    } else {
+        let jsonSchemaObject = JSON.parse(jsonSchema);
+        // Avoid nested reference (cannot be resolved by VSCode)
+        jsonSchemaObject['properties']['parameters']['items']['properties']['parameter_state_type']['$ref'] = '#/properties/inputs/items/properties/signal_types/items';
+        fs.writeFile(outputFileName, JSON.stringify(jsonSchemaObject, null, 2), 'utf8', (err) => {
+          if (err) throw err;
+          console.log('The file has been saved!');
+        });
+    }
 });
 
 
