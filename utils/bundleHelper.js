19f0277c142fa832bf167c482bf3d0b9a0c874f2
Enrico Eberhard <enrico.eberhard@gmail.com>
2025-01-09 17:25:41 +0100

    fix bundle helper

--- utils/bundleHelper.js
+++ utils/bundleHelper.js
@@ -9,18 +9,20 @@ $RefParser.bundle(filePath).then(async (schema) => {
     let jsonSchema = decodeURI(JSON.stringify(schema, null, 2));
     jsonSchema = jsonSchema.replaceAll('%24', '$');
 
-    if(outputFileName === "application.schema.json") {
+    if (outputFileName === "application.schema.json") {
         fs.writeFile(outputFileName, jsonSchema, 'utf8', (err) => {
-              if (err) throw err;
-              console.log('The file has been saved!');
+            if (err) throw err;
+            console.log('The file has been saved!');
         });
     } else {
         let jsonSchemaObject = JSON.parse(jsonSchema);
         // Avoid nested reference (cannot be resolved by VSCode)
-        jsonSchemaObject['properties']['parameters']['items']['properties']['parameter_state_type']['$ref'] = '#/properties/inputs/items/properties/signal_types/items';
+        if (jsonSchemaObject?.['properties']?.['parameters']?.['items']?.['properties']?.['parameter_state_type']?.['$ref']) {
+            jsonSchemaObject['properties']['parameters']['items']['properties']['parameter_state_type']['$ref'] = '#/properties/inputs/items/properties/signal_types/items';
+        }
         fs.writeFile(outputFileName, JSON.stringify(jsonSchemaObject, null, 2), 'utf8', (err) => {
-          if (err) throw err;
-          console.log('The file has been saved!');
+            if (err) throw err;
+            console.log('The file has been saved!');
         });
     }
 });
